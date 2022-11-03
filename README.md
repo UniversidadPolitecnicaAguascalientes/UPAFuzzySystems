86242af11f57022bb4c68c4088bfa940f36b8d61
Universidad Politécnica de Aguascalientes <107576914+UPAGIT@users.noreply.github.com>
2022-11-03 14:44:52 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# UPAFuzzySystems
+UPAFuzzySystems library that allows defining Fuzzy Inference Systems for different applications with continuous and discrete universes, it also deploys structures for the simulation of fuzzy control with transfer functions and state space models.
---
+++ build/lib/UPAFuzzySystems/__init__.py
@@ -0,0 +1,7 @@
+from .UPAFuzzySystems import fuzzy_universe
+from .UPAFuzzySystems import fuzzy_controller
+from .UPAFuzzySystems import inference_system
+from .UPAFuzzySystems import trapmf
+from .UPAFuzzySystems import trimf
+from .UPAFuzzySystems import gaussmf
+from .UPAFuzzySystems import gbellmf

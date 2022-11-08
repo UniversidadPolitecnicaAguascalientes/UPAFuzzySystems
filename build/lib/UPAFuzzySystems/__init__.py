59fe819b5822bfce602ffb14d24787e79e4fd3ea
Universidad Politécnica de Aguascalientes <107576914+UPAGIT@users.noreply.github.com>
2022-11-08 10:53:16 -0600

    Delete build/lib/UPAFuzzySystems directory

--- build/lib/UPAFuzzySystems/__init__.py
+++
@@ -1,7 +0,0 @@
-from .UPAFuzzySystems import fuzzy_universe
-from .UPAFuzzySystems import fuzzy_controller
-from .UPAFuzzySystems import inference_system
-from .UPAFuzzySystems import trapmf
-from .UPAFuzzySystems import trimf
-from .UPAFuzzySystems import gaussmf
-from .UPAFuzzySystems import gbellmf
